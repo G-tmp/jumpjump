c1f65fa029a5f89d52c77e87370b4dc070550222
G-tmp <55421901+G-tmp@users.noreply.github.com>
2025-08-14 00:52:09 +0800

    all_urls => dynamic urls

--- background.js
+++ background.js
@@ -1,32 +1,53 @@
 let redirectRules = [];
 
-function updateRules() {
-  browser.storage.local.get("rules").then((data) => {
-    redirectRules = data.rules || [];
-    console.log("Rules updated:", redirectRules);
-  });
-}
-
-// Listen for changes to storage and reload rules
-browser.storage.onChanged.addListener(updateRules);
 
 // Initial load
-updateRules();
+updateRules().then(updateListener);
 
-browser.webRequest.onBeforeRequest.addListener(
-  (details) => {
-    const url = new URL(details.url);
 
-    for (let rule of redirectRules) {
-      if (url.hostname === rule.from && url.pathname.includes(rule.match)) {
-        const newUrl = details.url.replace(rule.from + rule.match, rule.to) + rule.parameter;
-        console.log(`${url} => ${newUrl}`);
+async function updateRules() {
+  const data = await browser.storage.local.get("rules");
+  redirectRules = data.rules || [];
+  console.log("Rules updated:", redirectRules);
+}
 
-        browser.tabs.update(details.tabId, { url: newUrl });
-        return { cancel: true };
-      }
+
+function updateListener() {
+  // Remove old listener first
+  browser.webRequest.onBeforeRequest.removeListener(handleRequest);
+
+  // Convert rules into filters
+  const urls = redirectRules.filter(rule => rule.form != "").map(rule => `*://${rule.from}/*`);
+  console.log("match urls:", urls);
+
+  // Add new listener
+  if (urls.length > 0) {
+    browser.webRequest.onBeforeRequest.addListener(
+      handleRequest,
+      { urls },
+      ["blocking"]
+    );
+  }
+}
+
+
+// Listen for changes to storage and reload rules
+browser.storage.onChanged.addListener(async () => {
+  await updateRules();
+  updateListener();
+});
+
+
+function handleRequest(details) {
+  const url = new URL(details.url);
+
+  for (let rule of redirectRules) {
+    if (url.hostname === rule.from && url.pathname.includes(rule.match)) {
+      const newUrl = details.url.replace(rule.from + rule.match, rule.to) + rule.parameter;
+      console.log(`${url} => ${newUrl}`);
+
+      browser.tabs.update(details.tabId, { url: newUrl });
+      return { cancel: true };
     }
-  },
-  { urls: ["<all_urls>"] },
-  ["blocking"]
-);
+  }
+}
